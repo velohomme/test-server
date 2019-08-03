95295c5027647a0b9e29179ceb01ac41a93e2c6c
JimGeppert <jim.geppert@docusign.com>
2019-08-03 09:05:23 -0500

    fix type

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var app = express();
 const PORT = process.env.PORT || 5000;
 
 // this origin property causes the response to NOT use a wildcard for
-// Access-Control-Allow-Origin.  Rather it will return the host of the 
+// Access-Control-Allow-Origin.  Rather it will return the host of the
 // page which invoked the request, e.g., Access-Control-Allow-Origin: http://localhost:8055
- Access-Control-Allow-Origin
+app.use(
   cors({
     origin: function(origin, callback) {
       return callback(null, true);
