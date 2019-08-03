d636ac19fc19973224407cecce410cdeefcc4f76
JimGeppert <jim.geppert@docusign.com>
2019-08-03 08:46:30 -0500

    limit origin

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ var cors = require("cors");
 var app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+var allowedOrigins = ["http://localhost:8055"];
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 app.options("*", cors()); // enable pre-flight request
 
